e9bbf5ff6819cc576a98f00bec4ee65ab73faba0
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-05-22 15:16:07 +0300

    First reducer for the initial data of the application.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+
+export default function() {
+    return [
+        { title: 'Jabacript: The Good Parts'},
+        { title: 'Hairy Pothead'},
+        { title: 'The Dark Flower'},
+        { title: 'Banana Enforcer'},
+        { title: 'Pterodactyl Every Day Guide'}
+    ]
+}
