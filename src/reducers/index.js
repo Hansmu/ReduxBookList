94ea61c77532da44829b22e692deb0a3d117d73f
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-05-22 16:53:04 +0300

    Added reducer for selected book.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
+  activeBook: ActiveBook,
   listOfBooks: BooksReducer //Key is the piece of state and the value is the reducer itself
   //Reducer gets bound here to the key. You use state.listOfBooks to call this reducer in wherever you need.
 });
